29f3c1f6ba8ee0b154a5b87005eb9d61af825a14
Ryan Mobley <39319087+kj0317@users.noreply.github.com>
2020-01-09 21:13:25 -0700

    Added JAWS

--- new-kali-install.sh
+++ new-kali-install.sh
@@ -10,7 +10,6 @@ echo "Done..."
 echo "Downloading and installing pip3..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-echo "pip3 should now be a usable command..."
 cd AutoRecon/
 echo "Installing AutoRecon..."
 pip3 install -r requirements.txt
@@ -24,10 +23,13 @@ mkdir -v linux-pe-scripts
 cp linux-exploit-suggester/linux-exploit-suggester.sh linux-pe-scripts/
 cp LinEnum/LinEnum.sh linux-pe-scripts/
 cp unix-privesc-check/upc.sh linux-pe-scripts/
-echo "Done..."
+echo "Done"
 echo "Downloading MS17-010 tools..."
 git clone https://github.com/worawit/MS17-010.git
 git clone https://github.com/3ndG4me/AutoBlue-MS17-010.git
+echo "Downloading JAWS..."
+git clone https://github.com/411Hall/JAWS.git
+echo "Done."
 echo "Done...cleaning up."
 rm -r linux-exploit-suggester,LinEnum,unix-privesc-check
 echo "Verify all your tools are installed and work properly. 
